c455abf7767ec17fa6d6c28b96d0805a0a0d8154
Nathan <ng00367@surrey.ac.uk>
2021-03-28 13:26:27 -0700

    Added the ability to create businesses

--- controller/businesses.js
+++ controller/businesses.js
@@ -1,48 +1,49 @@
-const Business = require('../models/business')
-
+const Business = require("../models/business");
 
 //Route to get the businesses from the databases
-exports.getBusinesses = async (req,res,next) => {
+exports.getBusinesses = async (req, res, next) => {
   try {
     const businesses = await Business.find();
+    
 
     return res.status(200).json({
       success: true,
       count: businesses.length,
-      data: businesses
+      data: businesses,
     });
-  }
-  catch (err){
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
-}
+};
 
 //Route to post new businesses to the database
-exports.postBusinesses = async (req,res,next) => {
+exports.postBusinesses = async (req, res, next) => {
   try {
-    const business = await Business.create({ businessName: req.body.businessName, businessOwner: req.body.businessOwner /**req.user.ObjectId */, address: req.body.address});
-    
-    return res.status(200).json({
-      success: true,
-      data: business
-    })
-  }
-  catch (err){
+    const business = await Business.create({
+      businessName: req.body.businessName,
+      businessOwner: req.user._id ,
+      address: req.body.address,
+    });
+
+    res.redirect('/businesses')
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
-//Route to open the map 
-exports.openBusinesses = async (req,res,next) => {
+exports.createBusinesses = (req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.render("business/addBusiness", { title: "Add Business" });
+};
+
+//Route to open the map
+exports.openBusinesses = async (req, res, next) => {
   try {
-    const businesses = await Business.find();
-
-    res.render('business/overview', { title: 'Overview'})
-    
-  }
-  catch (err){
+    res.locals.currentUser = req.user;
+    res.render("business/overview", { title: "Overview" });
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
-}
+};
--- routes/businesses.js
+++ routes/businesses.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
-const { getBusinesses, postBusinesses, openBusinesses } = require('../controller/businesses');
+const { getBusinesses, postBusinesses, openBusinesses, createBusinesses } = require('../controller/businesses');
 
-router.get('/businesses', openBusinesses)
+router.get('/businesses', ensureAuthenticated, openBusinesses)
 
 router.get('/getbusinesses', getBusinesses)
+
+router.get('/createbusiness',ensureAuthenticated, createBusinesses)
 
 router.post('/businesses', postBusinesses)
 
